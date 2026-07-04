682ab4175280494f1bb1083700ba51d0c52f1d11
black <black@example.com>
2026-07-04 09:54:19 +0500

    Call greetWhite and greetBlack

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-def main():
-    print("hello everyone")
-
-if __name__ == "__main__":
-    main()
-
 def greetWhite():
     print("hello white")
 
 def greetBlack():
     print("hello black")
+
+def main():
+    print("hello everyone")
+    greetWhite()
+    greetBlack()
+
+if __name__ == "__main__":
+    main()
